a521a1b514873593266c7e65c38172cba9a09929
mrinalgosain <mrinal.gosain@mailchimp.com>
2022-04-28 11:07:50 -0700

    final test

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+# test_app.py
+
+def test_capitalize_string2():
+    """ test 11"""
+    s = "helloworld"
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.capitalize()
+
+def test_capitalize_string():
+    """ test 2"""
+    assert capitalize_string('test') == 'Test'
